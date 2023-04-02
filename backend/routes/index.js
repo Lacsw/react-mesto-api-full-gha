e0b849fdd07eeb00bb1b8645a4071e8829fd6894
Roman Frolov <frolloff1@yandex.ru>
2023-04-02 15:19:46 +0300

    feat: добавить краштест для тестирования восстановления

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,6 +7,12 @@ const usersRouter = require('./users');
 const cardsRouter = require('./cards');
 const auth = require('../middlewares/auth');
 
+router.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 router.use('/', authRouter);
 router.use(auth);
 router.post('/signout', (req, res) => {
